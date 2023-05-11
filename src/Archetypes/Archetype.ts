213be99eb1c5f189a66702dd8701feeabb3d3991
Willian <willian.amorim1@outlook.com>
2023-05-11 17:51:58 -0300

    adicionado classe abstrata de Archetype

---
+++ src/Archetypes/Archetype.ts
@@ -0,0 +1,33 @@
+import { EnergyType } from '../Energy';
+
+abstract class Archetype {
+  private _name: string;
+
+  constructor(
+    name:string,
+    private _special = 0,
+    private _cost = 0,
+  ) {
+    this._name = name;
+  }
+
+  get name():string {
+    return this._name;
+  }
+
+  get special():number {
+    return this._special;
+  }
+
+  get cost():number {
+    return this._cost;
+  }
+
+  static createdArchetypeInstances():number {
+    throw new Error('Not implemented');
+  }
+
+  abstract get energyType():EnergyType;
+}
+
+export default Archetype;
