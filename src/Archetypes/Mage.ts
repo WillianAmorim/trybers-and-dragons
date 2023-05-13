7fac1989b28d03c3a59deba2892447453a6251dd
Willian <willian.amorim1@outlook.com>
2023-05-13 12:16:25 -0300

    adicionado tipagem e energy recebendo valor no constructor

--- src/Archetypes/Mage.ts
+++ src/Archetypes/Mage.ts
@@ -2,11 +2,12 @@ import { EnergyType } from '../Energy';
 import Archetype from './Archetype';
 
 class Mage extends Archetype {
-  private _energyType = 'mana';
+  private _energyType: EnergyType;
   private static _count = 0;
 
   constructor(name:string) {
     super(name);
+    this._energyType = 'mana';
     Mage._count += 1;
   }
 
