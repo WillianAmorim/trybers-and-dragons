8b73256a6b103a3a9451dbdaf1c96307e51ecc7a
Willian <willian.amorim1@outlook.com>
2023-05-13 12:16:48 -0300

    adicionado tipagem e energy recebendo valor no constructor

--- src/Archetypes/Ranger.ts
+++ src/Archetypes/Ranger.ts
@@ -2,11 +2,12 @@ import { EnergyType } from '../Energy';
 import Archetype from './Archetype';
 
 class Ranger extends Archetype {
-  private _energyType = 'stamina';
+  private _energyType: EnergyType;
   private static _count = 0;
 
   constructor(name:string) {
     super(name);
+    this._energyType = 'stamina';
     Ranger._count += 1;
   }
 
